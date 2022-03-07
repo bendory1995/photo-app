da2d46f5df19a122e7b6b684ffb36d06d6234e03
Alex Oh <oalex@knights.ucf.edu>
2022-03-07 11:44:10 -0500

    Initial commit

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import {initializeApp, getApps, getApp} from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+import {getStorage} from "firebase/storage"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAtX31-MUw1DOiitQ0b17psUlKBMnL-xSA",
+    authDomain: "interactive-journal-cop4331.firebaseapp.com",
+    projectId: "interactive-journal-cop4331",
+    storageBucket: "interactive-journal-cop4331.appspot.com",
+    messagingSenderId: "414492863642",
+    appId: "1:414492863642:web:29df146ef5a8f113798323",
+    measurementId: "G-XJJD98RWWF"
+  };
+
+  const app = !getApps().length ? initializeApp(firebaseConfig): getApp();
+  const db = getFirestore();
+  const storage = getStorage();
+
+  export {app, db, storage};
---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import Header from "../components/Header";
+import Feed from "../components/Feed";
+import Modal from "../components/Modal";
+export default function Home() {
+  return (
+    <div className="bg-red-50 h-screen overflow-y-scroll scrollbar-hide">
+      <Head>
+        <title>Interactive Diary</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* HEADER */}
+      <Header />
+      <Feed />
+      <Modal />
+      {/* <Feed />  */}
+      {/* FEED */}
+      {/* Modal */}
+    </div>
+  );
+}
